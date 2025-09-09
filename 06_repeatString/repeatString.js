d1961f1cbaaa669bec1e6bafce39e78bba6eef3f
Mushab Ambulo <Mushabpasod23@gmail.com>
2025-09-09 22:30:02 +0800

    repeatString updaate

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
-};
-
-// Do not edit below this line
-module.exports = repeatString;
+const repeatString = function(string, num) {
+    if (num < 0) return 'ERROR'; // handle negative numbers
+  
+    let result = ''; // start with an empty string
+  
+    for (let i = 0; i < num; i++) {
+      result += string; // append the string on each loop
+    }
+  
+    return result;
+  };
+  
+  // Do not edit below this line
+  module.exports = repeatString;
+  
