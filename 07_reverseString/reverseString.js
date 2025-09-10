a801484c8904d665bb163aac97d56ced435c6776
Mushab Ambulo <Mushabpasod23@gmail.com>
2025-09-10 11:55:00 +0800

    Reverse update

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
-};
-
-// Do not edit below this line
-module.exports = reverseString;
+const reverseString = function(string) {
+    let reversed = '';
+    for (let i = string.length - 1; i >= 0; i--) {
+      reversed += string[i];
+    }
+    return reversed;
+  };
+  
+  // Do not edit below this line
+  module.exports = reverseString;
+  
