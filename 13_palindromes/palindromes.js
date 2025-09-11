5322093825f5baffa5b91d8333bdb5183ece0488
Mushab Ambulo <Mushabpasod23@gmail.com>
2025-09-11 22:17:57 +0800

    palindrome

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,11 @@
-const palindromes = function () {
-
-};
-
-// Do not edit below this line
-module.exports = palindromes;
+const palindromes = function (str) {
+    // Normalize: lowercase and remove non-alphanumeric chars
+    const cleaned = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+    
+    // Check if cleaned string is the same reversed
+    return cleaned === cleaned.split('').reverse().join('');
+  };
+  
+  // Do not edit below this line
+  module.exports = palindromes;
+  
